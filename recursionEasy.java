b8dffe57c6497c4eccb28acc007c894f475244f9
anidevl <aniketwork200322@gmail.com>
2024-07-07 17:54:25 +0530

    Recursion easy fully complete

--- recursionEasy.java
+++ recursionEasy.java
@@ -74,7 +74,130 @@ public class recursionEasy{
     }
     */
     
-    // 
+    // Q5. Print the fibonacci sequence till nth term.
+    /*
+    public static void printfibo(int a,int b,int n){
+        //Base condition
+        if(n<0){
+           return;
+        }
+        System.out.print(" "+b+" ");
+        int c=a+b;
+        printfibo(b,c,n-1);
 
+
+
+
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value for nth term:");
+        int n=sc.nextInt();
+        int a=0,b=1;
+        if(n<=0){
+            System.out.println("Please Enter any Natural Number");
+        }
+        else if(n==1){
+            System.out.println("0");
+        }
+        else if(n==2){
+            System.out.println("0 1");
+        }
+        else{
+            System.out.print("0 ");
+            printfibo(a,b,n);
+
+        }
+         
+    }
+    */
+    /*
+    // More optimised version of the above Fibo code
+    public static void printFibo(int a ,int b,int n){
+        if(n==0){
+            return;
+        }
+        int c= a+b;
+        System.out.print(c+" ");
+        printFibo(b,c,n-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter nth value term:");
+        int n= sc.nextInt();
+        int a=0,b=1;
+        if(n<=0){
+            System.out.println("Please Enter a valid Natural Number:");
+        }
+        else if(n==1){
+            System.out.println("0");
+        }
+        else if(n==2){
+            System.out.println("0 1");
+        }
+        else{
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        printFibo(a,b,n-2);
+    }
+  }
+  */
+  /*
+   //Q6. Print x^n (with stack height = n)
+    public static int calcPower(int x,int n){
+        if(n==0) //base case 1
+        {
+            return 1;
+        }
+        if(x==0) //base case 2
+        {
+            return 0;
+        }
+        //kaam
+        int xpown= x*calcPower(x,n-1);
+        return xpown;
+
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for x:");
+        int x = sc.nextInt();
+        System.out.println("Enter the value for n:");
+        int n = sc.nextInt();
+        System.out.println(calcPower(x,n)); 
+
+    }
+    */
+    // Q7. Print x^n (with stack height = logn) ------> More optimised version of above question no. 6
+    public static int calcPower(int x,int n){
+        if(n==0) //base case 1
+        {
+            return 1;
+        }
+        if(x==0) //base case 2
+        {
+            return 0;
+        }
+        //kaam
+        if(n%2==0) //if given value of n in x^n is even
+        {
+            return calcPower(x,n/2) * calcPower(x,n/2);
+        }
+        else //if given n value is odd
+        {
+            return calcPower(x,n/2) * calcPower(x,n/2) * x;
+        }
+        
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value for x:");
+        int x = sc.nextInt();
+        System.out.println("Enter the value for n:");
+        int n = sc.nextInt();
+        System.out.println(calcPower(x,n)); 
+
+    }
 
 }
